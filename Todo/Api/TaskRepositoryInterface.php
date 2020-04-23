2e6ce17dfe7f46fec6f29d9cd5911e1dba769b73
Victor Shust <victor.shust@goodahead.com>
2020-04-24 00:48:36 +0300

    [Homework 35] Added API Data interfaces

--- Todo/Api/TaskRepositoryInterface.php
+++ Todo/Api/TaskRepositoryInterface.php
@@ -2,11 +2,14 @@
 
 namespace MageMastery\Todo\Api;
 
+use MageMastery\Todo\Api\Data\TaskSearchResultInterface;
+use Magento\Framework\Api\SearchResultsInterface;
+
 /**
  * @api
  */
 interface TaskRepositoryInterface
 {
     public function get(int $taskId);
-    public function getList();
+    public function getList(SearchResultsInterface $searchResults): TaskSearchResultInterface;
 }
