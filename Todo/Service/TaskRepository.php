6fcdecfc9068f8e5e29ffc9f980769dfc4cdf0a8
Victor Shust <victor.shust@goodahead.com>
2020-04-27 00:43:00 +0300

    [Homework 36] Added logic for getList function

--- Todo/Service/TaskRepository.php
+++ Todo/Service/TaskRepository.php
@@ -2,9 +2,13 @@
 
 namespace MageMastery\Todo\Service;
 
+use MageMastery\Todo\Api\Data\TaskSearchResultInterface;
+use MageMastery\Todo\Api\Data\TaskSearchResultInterfaceFactory;
 use MageMastery\Todo\Api\TaskRepositoryInterface;
 use MageMastery\Todo\Model\ResourceModel\Task;
 use MageMastery\Todo\Model\TaskFactory;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
+use Magento\Framework\Api\SearchResultsInterface;
 
 class TaskRepository implements TaskRepositoryInterface
 {
@@ -18,12 +22,33 @@ class TaskRepository implements TaskRepositoryInterface
      */
     protected $taskFactory;
 
+    /**
+     * @var CollectionProcessorInterface
+     */
+    protected $collectionProcessor;
+
+    /**
+     * @var TaskSearchResultInterfaceFactory
+     */
+    protected $searchResultInterfaceFactory;
+
+    /**
+     * TaskRepository constructor.
+     * @param Task $resource
+     * @param TaskFactory $taskFactory
+     * @param CollectionProcessorInterface $collectionProcessor
+     * @param TaskSearchResultInterfaceFactory $searchResultInterfaceFactory
+     */
     public function __construct(
         Task $resource,
-        TaskFactory $taskFactory
+        TaskFactory $taskFactory,
+        CollectionProcessorInterface $collectionProcessor,
+        TaskSearchResultInterfaceFactory $searchResultInterfaceFactory
     ) {
         $this->resource = $resource;
         $this->taskFactory = $taskFactory;
+        $this->collectionProcessor = $collectionProcessor;
+        $this->searchResultInterfaceFactory = $searchResultInterfaceFactory;
     }
 
     public function get(int $taskId)
@@ -33,7 +58,13 @@ class TaskRepository implements TaskRepositoryInterface
         return $object;
     }
 
-    public function getList()
+    public function getList(SearchResultsInterface $searchCriteria): TaskSearchResultInterface
     {
+        $searchResult = $this->searchResultInterfaceFactory->create();
+        $searchResult->setSearchCriteria($searchCriteria);
+
+        $this->collectionProcessor->process($searchCriteria, $searchResult);
+
+        return $searchResult;
     }
 }
