069958310a4026a44218fbb2688bb2deec1bc291
Victor Shust <victor.shust@goodahead.com>
2020-05-23 23:32:12 +0300

    [Homework 42] Added logic for save and delete tasks in DB

--- Todo/Controller/Index/Index.php
+++ Todo/Controller/Index/Index.php
@@ -2,6 +2,7 @@
 
 namespace MageMastery\Todo\Controller\Index;
 
+use MageMastery\Todo\Api\TaskManagementInterface;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
@@ -12,25 +13,53 @@ use Magento\Framework\Api\SearchCriteriaBuilder;
 
 class Index extends Action
 {
+    /**
+     * @var ResourceTask
+     */
     protected $taskResource;
 
+    /**
+     * @var TaskFactory
+     */
     protected $taskFactory;
 
+    /**
+     * @var TaskRepository
+     */
     protected $taskRepository;
 
+    /**
+     * @var SearchCriteriaBuilder
+     */
     protected $searchCriteriaBuilder;
 
+    /**
+     * @var TaskManagementInterface
+     */
+    protected $taskManagement;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param TaskFactory $taskFactory
+     * @param ResourceTask $taskResource
+     * @param TaskRepository $taskRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param TaskManagementInterface $taskManagement
+     */
     public function __construct(
         Context $context,
         TaskFactory $taskFactory,
         ResourceTask $taskResource,
         TaskRepository $taskRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        TaskManagementInterface $taskManagement
     ) {
         $this->taskFactory = $taskFactory;
         $this->taskResource = $taskResource;
         $this->taskRepository = $taskRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->taskManagement = $taskManagement;
 
         parent::__construct($context);
     }
@@ -38,6 +67,11 @@ class Index extends Action
     public function execute()
     {
         $tasks = $this->taskRepository->getList($this->searchCriteriaBuilder->create())->getItems();
+
+        $task = $this->taskRepository->get(1);
+        $task->setData('status', 'complete');
+
+        $this->taskManagement->save($task);
 
         var_dump($tasks);
         return;
