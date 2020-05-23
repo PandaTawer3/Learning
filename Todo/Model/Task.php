507a9e7b3a48890498473df5f7128a1d2045e2c5
Victor Shust <victor.shust@goodahead.com>
2020-05-23 23:53:16 +0300

    [Homework 43] Added fields to return during API call

--- Todo/Model/Task.php
+++ Todo/Model/Task.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MageMastery\Todo\Model;
 
 use MageMastery\Todo\Api\Data\TaskInterface;
@@ -8,8 +10,36 @@ use MageMastery\Todo\Model\ResourceModel\Task as ResourceTask;
 
 class Task extends AbstractModel implements TaskInterface
 {
+    const TASK_ID = 'task_id';
+    const STATUS = 'status';
+    const LABEL = 'label';
+
     protected function _construct()
     {
         $this->_init(ResourceTask::class);
+    }
+
+    /**
+     * @return int
+     */
+    public function getTaskId(): int
+    {
+        return (int)$this->getData(self::TASK_ID);
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->getData(self::STATUS);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel(): string
+    {
+        return $this->getData(self::LABEL);
     }
 }
